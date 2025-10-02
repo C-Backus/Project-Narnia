67718877728ca556dd833ecc01e0081582fe685b
C-Backus <cbackus@uccs.edu>
2025-10-02 12:23:33 -0600

    Update ssh.py
    updated to use SSH Key

--- flask-ui/ssh.py
+++ flask-ui/ssh.py
@@ -3,13 +3,14 @@ import paramiko
 def list_files():
     hostname = "localhost"
     port = 2222
-    username = ""    #username (not here for security)
-    password = ""   #password (not here for security)
+    username = ""    #username (not filled for security)
+    key_path = "" #using ssh key (not filled for security)
 
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname, port=port, username=username, password=password)
+        private_key = paramiko.Ed25519Key(filename=key_path)
+        ssh.connect(hostname, port=port, username=username, pkey=private_key)
        
        
         #this runs command to lsit all folders in home directory
