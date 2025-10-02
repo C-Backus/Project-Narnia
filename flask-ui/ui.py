7a59093f47bb5f64cb3f4d2ffcd39518e0078631
C-Backus <cbackus@uccs.edu>
2025-10-02 11:11:29 -0600

    Update ui.py
    added file display from local wsl via ssh

--- flask-ui/ui.py
+++ flask-ui/ui.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from ssh import list_files
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # loads templates/index.html
+    files = list_files()   #for ssh file list
+    return render_template('index.html', files=files)  # loads templates/index.html
 
 if __name__ == '__main__':
     app.run(debug=True)
